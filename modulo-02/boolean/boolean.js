e45ebcf6115d26b2ebd81e8e6280647a1bbe4616
unknown <ttx8000@gmail.com>
2023-08-08 22:08:26 -0300

    modulo2

---
+++ modulo-02/boolean/boolean.js
@@ -0,0 +1,11 @@
+"use strict";
+//
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+//
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(`É par ? ${isEven(11)}`);
+console.log(`É par ? ${isEven(2)}`);
---
+++ modulo-02/boolean/boolean.ts
@@ -0,0 +1,11 @@
+//
+let tarefaConcluida: boolean = true
+let tarefaPendente: boolean = false
+console.log(tarefaConcluida)
+
+//
+function isEven(num: number): boolean {
+    return num % 2 === 0
+}
+console.log(`É par ? ${isEven(11)}`)
+console.log(`É par ? ${isEven(2)}`)
